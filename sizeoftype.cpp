00de1ad8b3e78ebf02b446df1b7cfa4125ffa485
AMAN KUMAR GUPTA <akg42488@gmail.com>
2021-05-13 18:05:52 +0530

    program with output ss

---
+++ sizeoftype.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int integer;
+	char ch;
+	float f;
+	double d;
+	
+	cout << "Size of int is: " <<
+	sizeof(integer) <<"\n";
+
+	cout << "Size of char is: " <<
+	sizeof(ch) <<"\n";
+	
+	cout << "Size of float is: " <<
+	sizeof(f) <<"\n";
+
+	cout << "Size of double is: " <<
+	sizeof(d) <<"\n";
+
+	return 0;
+}
+
